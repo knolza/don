5f3159062cb1be02972e5db75a7775a6c5874398
yezune, choi <yezune@gmail.com>
2016-07-25 23:42:25 +0900

    remove checkpoints

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -35,7 +35,8 @@ namespace Checkpoints {
 
         MapCheckpoints::const_iterator i = checkpoints.find(nHeight);
         if (i == checkpoints.end()) return true;
-        return hash == i->second;
+        //return hash == i->second;
+        return true;
     }
 
     //! Guess how far we are in the verification process at the given block index
@@ -77,7 +78,8 @@ namespace Checkpoints {
 
         const MapCheckpoints& checkpoints = *Params().Checkpoints().mapCheckpoints;
 
-        return checkpoints.rbegin()->first;
+        //return checkpoints.rbegin()->first;
+        return 0;
     }
 
     CBlockIndex* GetLastCheckpoint()
@@ -92,7 +94,8 @@ namespace Checkpoints {
             const uint256& hash = i.second;
             BlockMap::const_iterator t = mapBlockIndex.find(hash);
             if (t != mapBlockIndex.end())
-                return t->second;
+                //return t->second;
+                return NULL;
         }
         return NULL;
     }
